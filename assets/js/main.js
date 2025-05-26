b955769a258a698477579b4d7c62aa7bdb5f12a6
CHEANGCHANG <jrcheangmomin@gmail.com>
2025-05-26 14:54:26 +0530

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,216 @@
+(function ($) {
+    "use strict";
+
+    $(document).ready(function($){
+        
+        // testimonial sliders
+        $(".testimonial-sliders").owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            responsive:{
+                0:{
+                    items:1,
+                    nav:false
+                },
+                600:{
+                    items:1,
+                    nav:false
+                },
+                1000:{
+                    items:1,
+                    nav:false,
+                    loop:true
+                }
+            }
+        });
+
+        // homepage slider
+        $(".homepage-slider").owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            nav: true,
+            dots: false,
+            navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+            responsive:{
+                0:{
+                    items:1,
+                    nav:false,
+                    loop:true
+                },
+                600:{
+                    items:1,
+                    nav:true,
+                    loop:true
+                },
+                1000:{
+                    items:1,
+                    nav:true,
+                    loop:true
+                }
+            }
+        });
+
+        // logo carousel
+        $(".logo-carousel-inner").owlCarousel({
+            items: 4,
+            loop: true,
+            autoplay: true,
+            margin: 30,
+            responsive:{
+                0:{
+                    items:1,
+                    nav:false
+                },
+                600:{
+                    items:3,
+                    nav:false
+                },
+                1000:{
+                    items:4,
+                    nav:false,
+                    loop:true
+                }
+            }
+        });
+
+        // count down
+        if($('.time-countdown').length){  
+            $('.time-countdown').each(function() {
+            var $this = $(this), finalDate = $(this).data('countdown');
+            $this.countdown(finalDate, function(event) {
+                var $this = $(this).html(event.strftime('' + '<div class="counter-column"><div class="inner"><span class="count">%D</span>Days</div></div> ' + '<div class="counter-column"><div class="inner"><span class="count">%H</span>Hours</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%M</span>Mins</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%S</span>Secs</div></div>'));
+            });
+         });
+        }
+
+        // projects filters isotop
+        $(".product-filters li").on('click', function () {
+            
+            $(".product-filters li").removeClass("active");
+            $(this).addClass("active");
+
+            var selector = $(this).attr('data-filter');
+
+            $(".product-lists").isotope({
+                filter: selector,
+            });
+            
+        });
+        
+        // isotop inner
+        $(".product-lists").isotope();
+
+        // magnific popup
+        $('.popup-youtube').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+            fixedContentPos: false
+        });
+
+        // light box
+        $('.image-popup-vertical-fit').magnificPopup({
+            type: 'image',
+            closeOnContentClick: true,
+            mainClass: 'mfp-img-mobile',
+            image: {
+                verticalFit: true
+            }
+        });
+
+        // homepage slides animations
+        $(".homepage-slider").on("translate.owl.carousel", function(){
+            $(".hero-text-tablecell .subtitle").removeClass("animated fadeInUp").css({'opacity': '0'});
+            $(".hero-text-tablecell h1").removeClass("animated fadeInUp").css({'opacity': '0', 'animation-delay' : '0.3s'});
+            $(".hero-btns").removeClass("animated fadeInUp").css({'opacity': '0', 'animation-delay' : '0.5s'});
+        });
+
+        $(".homepage-slider").on("translated.owl.carousel", function(){
+            $(".hero-text-tablecell .subtitle").addClass("animated fadeInUp").css({'opacity': '0'});
+            $(".hero-text-tablecell h1").addClass("animated fadeInUp").css({'opacity': '0', 'animation-delay' : '0.3s'});
+            $(".hero-btns").addClass("animated fadeInUp").css({'opacity': '0', 'animation-delay' : '0.5s'});
+        });
+
+       
+
+        // stikcy js
+        $("#sticker").sticky({
+            topSpacing: 0
+        });
+
+        //mean menu
+        $('.main-menu').meanmenu({
+            meanMenuContainer: '.mobile-menu',
+            meanScreenWidth: "992"
+        });
+        
+         // search form
+        $(".search-bar-icon").on("click", function(){
+            $(".search-area").addClass("search-active");
+        });
+
+        $(".close-btn").on("click", function() {
+            $(".search-area").removeClass("search-active");
+        });
+    
+    });
+
+
+    jQuery(window).on("load",function(){
+        jQuery(".loader").fadeOut(1000);
+    });
+
+
+}(jQuery));
+
+// squice
+let list = document.querySelectorAll('.carousel .list .item');
+let carousel = document.querySelector('.carousel');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+
+let mockup = document.querySelector('.mockup');
+
+let count = list.length;
+let active = 0;
+let leftMockup = 0;
+let left_each_item = 100 / (list.length - 1);
+
+
+next.onclick = () => {
+    active = active >= count - 1 ? 0 : active + 1;
+    leftMockup = leftMockup + left_each_item;
+    carousel.classList.remove('right');
+    changeCarousel();
+}
+prev.onclick = () => {
+    active = active <= 0 ? count - 1 : active - 1;
+    leftMockup = leftMockup - left_each_item;
+    carousel.classList.add('right');
+    changeCarousel();
+}
+function changeCarousel() {
+    // find item has class hidden to remove it
+    let hidden_old = document.querySelector('.item.hidden');
+    if(hidden_old) hidden_old.classList.remove('hidden');
+
+    // find item old active to remove it and add class hidden
+    let active_old = document.querySelector('.item.active');
+    active_old.classList.remove('active');
+    active_old.classList.add('hidden');
+    // add class active in position active new
+    list[active].classList.add('active');
+    // change mockup background
+    mockup.style.setProperty('--left', leftMockup + '%');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(()=> {next.click()}, 3000);
+}
+
+
+// auto run 3s
+let refreshInterval = setInterval(()=> {next.click()}, 3000);
